ebd02cfcdfaa8aa7aa7fff0d67aad345336d26f7
aymen hmani <47940043+TheLime03@users.noreply.github.com>
2022-04-29 11:03:02 +0100

    Delete test file to be deleted.py

--- test file to be deleted.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-import time
-while True:
-    if pyautogui.locateOnScreen('opera.png') != None:
-        print("i see you")
-        time.sleep(0.5)
-    else:
-        print("i dont see you")
-        time.sleep(0.5)
